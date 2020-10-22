27d6efe6eb841b766d3d310c80371c137ed317f5
JMG <jmg@JMGs-Air.bbrouter>
2020-10-23 03:14:44 +0800

    all forms

---
+++ public/fb.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAYBiNWWdDIyiNtEEFbF_jZOa7p4MXR5qU",
+    authDomain: "cdrrmo-cis.firebaseapp.com",
+    databaseURL: "https://cdrrmo-cis.firebaseio.com",
+    projectId: "cdrrmo-cis",
+    storageBucket: "cdrrmo-cis.appspot.com",
+    messagingSenderId: "975570594314",
+    appId: "1:975570594314:web:ff9771d6ea39959cf38df3",
+    measurementId: "G-Q26QCYSEHB"
+};
+firebase.initializeApp(firebaseConfig);
--- public/login.js
+++ public/login.js
@@ -25,3 +25,32 @@ $('#login-form').on('submit', function(e) {
             });
     }
 });
+
+firebase.auth().onAuthStateChanged(function(user) {
+
+    if (user) { // if already logged in
+        if (user.emailVerified) {
+            window.location.href = './';
+        } else {
+            alert('Account not yet verify check your email!')
+        }
+    }
+});
+
+/*******************\
+ * init Login UI *
+\*******************/
+
+// FirebaseUI config.
+var uiConfig = {
+    'signInSuccessUrl': false,
+    'signInOptions': [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    'tosUrl': false,
+};
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
