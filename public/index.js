8db40f12293592b28f3f80850664864a3fb4c644
JMG <jmg@JMGs-Air.bbrouter>
2020-10-29 03:40:43 +0800

    data

--- public/index.js
+++ public/index.js
@@ -1,26 +1,50 @@
 $(document).ready(function() {
+    pageSetUp();
+
+    function testrun() {
+        alert('test')
+    }
+
+    $('.chonav').hide()
+    $('.depednav').hide()
+    $('.brgynav').hide()
+    $('.cswdonav').hide()
+    $('.cdrrmonav').hide()
+
+    $("ul.li").find(".chonav").css("background-color", "red");
 
     firebase.auth().onAuthStateChanged(function(user) {
+
         var ref = firebase.database().ref("users");
         ref.orderByChild("email").equalTo(user.email).once("child_added", function(snapshot) {
             console.log('users data', snapshot.val())
 
             if (snapshot.val().role) {
-                if (snapshot.val().role === 0) {
-
-                } else if (snapshot.val().role === 1) {
-
-                } else if (snapshot.val().role === 2) {
-
-                } else if (snapshot.val().role === 3) {
-
-                } else if (snapshot.val().role === 4) {
-
+                if (snapshot.val().role == 0) {
+                    $('.chonav').show()
+                    $('.depednav').show()
+                    $('.brgynav').show()
+                    $('.cswdonav').show()
+                    $('.cdrrmonav').show()
+                } else if (snapshot.val().role == 1) {
+                    $('.chonav').show()
+                } else if (snapshot.val().role == 2) {
+                    $('.depednav').show()
+                } else if (snapshot.val().role == 3) {
+                    $('.brgynav').show()
+                } else if (snapshot.val().role == 4) {
+                    $('.cswdonav').show()
+                } else if (snapshot.val().role == 4) {
+                    $('.cdrrmonav').show()
                 }
 
             }
 
-        });
+        })
+
+
+
+
         if (!user) {
             window.location.href = './login.html';
         } else {
