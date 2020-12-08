328c42c44440315c3838815ade1057298a8ab463
Geektech <jmlgrefaldo@bicol-u.edu.ph>
2020-12-09 00:30:15 +0800

    print change

--- public/contollers/cnsbdcholistCtrl.js
+++ public/contollers/cnsbdcholistCtrl.js
@@ -184,21 +184,42 @@ angular.module('newApp').controller('cnsbdcholistCtrl', function($firebaseArray,
 
 
         $('#printit').modal('show');
-
-
         setTimeout(function() {
+            // document.getElementById("btnPrint").onclick = function() {
+            printElement(document.getElementById("printThis"));
 
-            printJS({
-                printable: 'newcfp',
-                type: 'html',
-                targetStyles: ['*'],
-                maxWidth: 'auto'
-            })
+            var modThis = document.querySelector("#printSection .modifyMe");
+            modThis.appendChild(document.createTextNode(""));
 
-            $('#printit').modal('hide');
-        }, 1000);
+            window.print();
 
+
+
+            function printElement(elem) {
+
+
+                var domClone = elem.cloneNode(true);
+
+                var $printSection = document.getElementById("printSection");
+
+                if (!$printSection) {
+                    var $printSection = document.createElement("div");
+                    $printSection.id = "printSection";
+                    document.body.appendChild($printSection);
+                }
+
+                $printSection.innerHTML = "";
+
+                $printSection.appendChild(domClone);
+
+                $('#printit').modal('hide');
+
+            }
+
+
+        }, 100);
     };
+
 
     $scope.selectUser2 = function(users) {
         // console.log(users);
